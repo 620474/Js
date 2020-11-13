3072938ff01d4795e4fb5e8d74979a77989bac0f
620474 <jklooop@gmail.com>
2020-11-14 01:06:51 +0300

    2HW
    2HW

---
+++ 2HW/1.js
@@ -0,0 +1,99 @@
+console.log("Первая задача");
+let month = prompt("Введите месяц").toLowerCase();
+switch (month) {
+    case "январь":
+    case "декабрь":
+    case "февраль":
+        alert("Зима");
+        break;
+
+    case "март":
+    case "апрель":
+    case "май":
+        alert('Весна');
+        break;
+
+    case "июнь":
+    case "июль":
+    case "август":
+        alert('Лето');
+        break;
+
+    case "сентябрь":
+    case "октябрь":
+    case "ноябрь":
+        alert('Осень');
+        break;
+
+
+}
+console.log("Вторая задача");
+
+let length = prompt("Введите длину");
+let type = Number(prompt("Введите единицу измерения"));
+switch (type) {
+    case 1:
+        alert(`Длина ${length / 10} метров`);
+    case 2:
+        alert(`Длина ${length * 1000} метров`);
+        break;
+    case 3:
+        alert(`Длина ${length} метров`);
+        break;
+    case 4:
+        alert(`Длина ${length / 1000} метров`);
+        break;
+    case 5:
+        alert(`Длина ${length / 100} метров`);
+        break;
+}
+
+console.log("Третья задача");
+
+let number = prompt("Введите число");
+
+if (number >= 100 || number <= -100) {
+    alert(number < 0 ? "отрицательное трех значное число" : "положительное трехзначное число");
+} else if (number >= 10 && number <= 99 || number <= -10 && number <= -99) {
+    alert(number < 0 ? "отрицательное двухзначное число" : "положительное двухзначное число");
+} else if (number >= 1 && number <= 9 || number <= -1 && number <= -9) {
+    alert(number < 0 ? "отрицательное однозначное число" : "положительное однозначное число");
+} else {
+    alert("нулевое число");
+}
+
+console.log("четвертая задача");
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+        console.log("three");
+    } else if (i % 3 != 0 && i % 5 == 0) {
+        console.log("five");
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("threefive");
+    }
+}
+
+console.log("шестая задача");
+
+let bottle = prompt("Введите кол-во моющего средства");
+let numberOf = prompt("Введите кол-во тарелок");
+
+
+for (; numberOf >= 0 || bottle >= 0; numberOf--) {
+    if (numberOf === 0 && bottle === 0) {
+        console.log(`Все вымыто, осталось чистящего средства ${bottle}, тарелок ${numberOf}`);
+        break;
+    }
+    console.log(`Осталось моющего средства: ${bottle}`);
+    console.log(`Осталось тарелок: ${numberOf}`);
+    if (numberOf === 0 || bottle === 0) {
+        console.log(numberOf === 0 ? `Все тарелки вымыты, осталось моющего средства ${bottle}` : `Моющее средство закончилось, осталось грязных тарелок ${numberOf}`);
+        break;
+    }
+    bottle -= 0.5;
+}
+
+
+
+
