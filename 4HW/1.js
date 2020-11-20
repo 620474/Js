3cfc68172dee063cba580564928d409ff164176d
620474 <jklooop@gmail.com>
2020-11-20 21:28:42 +0300

    HW5
    HW5

--- 4HW/1.js
+++ 4HW/1.js
@@ -5,10 +5,11 @@ let array = String(a).split("");
 array.forEach(e => sum += parseInt(e));
 console.log(sum);
 console.log("Вторая задача");
-let b = "45545";
+let b = "4545";
+let c = "4";
 let array2 = String(b).split("");
 let res = [];
-array2.forEach(e => res.push(e + e));
+array2.forEach(e => e === c ? res.push(e + e) : res.push(e));
 console.log(res.toString().replace(/,/g, ''));
 
 
---
+++ 5HW/1.js
@@ -0,0 +1,78 @@
+console.log("1 задача");
+
+function compareTwoArrays(a, b) {
+    return a.length === b.length;
+}
+
+let a = [1, 2, 3, 4, 5, 6];
+let b = [1, 2, 5];
+console.log(compareTwoArrays(a, b));
+
+
+console.log("2 задача");
+
+function range(start, finish, step = 1) {
+    let arrayRange = [];
+    for (let i = start; i < finish; i += step) {
+        arrayRange.push(i);
+    }
+    return arrayRange;
+}
+
+console.log(range(1, 10, 2));
+
+
+console.log("3 задача");
+
+function profile(student) {
+    let write = `
+    name: ${student.name}
+    age: ${student.age}
+    hobbies : ${student.hobbies}
+    institute: ${student.institute}`;
+    document.write(write);
+}
+
+let student = {
+    name: "Karina",
+    age: 18,
+    hobbies: ["books", " anime", " Kpop"],
+    institute: "ITMO",
+}
+profile(student);
+console.log(profile(student));
+
+
+console.log("4 задача");
+
+let alphabet1 = "аеёиоуэюя";
+let alphabet2 = "бвгджзйклмнпрстфхцчшщъь";
+
+function rnd(alphabet) {
+    return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+}
+
+let text = "";
+let t = Math.floor(Math.random() * (2 - 1) + 1);
+if (t === 1) {
+    text += rnd(alphabet1);
+} else {
+    text += rnd(alphabet2);
+}
+for (let i = 2; i <= Math.floor(Math.random() * (6 - 3) + 3); i++) {
+    if (t === 1) {
+        if (i % 2 === 0) {
+            text += rnd(alphabet2);
+        } else {
+            text += rnd(alphabet1);
+        }
+    } else if (t === 2) {
+        if (i % 2 !== 0) {
+            text += rnd(alphabet1);
+        } else {
+            text += rnd(alphabet2);
+        }
+    }
+}
+console.log(text);
+
